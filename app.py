bab2592621412b281db274b8aee1750487825c5d
kinga112 <kinga112@yahoo.com>
2023-12-01 16:44:16 -0500

    trying aws bucket again

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+import os
 from flask import Flask, render_template, request, redirect
 import random
 import main
-import pyqrcode
+# import pyqrcode
 import string
+import segno
+import boto3
 
-app = Flask(__name__, static_url_path="/tmp", static_folder='tmp')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
@@ -44,9 +47,22 @@ def admin(id):
 @app.route('/create_queue/<id>', methods=['POST', 'GET'])
 def create_queue(id):
 
-    qrcode = pyqrcode.create('cyber-sequence.vercel.app/in_queue/id/{}'.format(id))
-    qrcode.png('/tmp/code{}.png'.format(id), scale=6, module_color=[0, 0, 0, 128], background=[0xFF,0xFF,0xFF])
-    qr_pic = '/tmp/code{}.png'.format(id)
+    # qrcode = pyqrcode.create('cyber-sequence.vercel.app/in_queue/id/{}'.format(id))
+    # qrcode.png('/tmp/code{}.png'.format(id), scale=6, module_color=[0, 0, 0, 128], background=[0xFF,0xFF,0xFF])
+    # qr_pic = '/tmp/code{}.png'.format(id)
+    # qr_pic.save('/tmp/code{}.png'.format(id))
+
+    qrcode = segno.make_qr('cyber-sequence.vercel.app/in_queue/id/{}'.format(id))
+    print("QRCODE: ", qrcode)
+    qrcode.save('/tmp/code{}.png'.format(id))
+
+    client = boto3.client('s3')
+    client.upload_file('/tmp/code{}.png'.format(id), 'queue-project', 'code{}.png'.format(id))
+    print("OKAY OKAY1")
+    client.download_file('queue-project', 'code{}.png'.format(id), '/tmp/code{}.png'.format(id))
+    print("OKAY OKAY2")
+    # print(open('/tmp/code{}.png'.format(id)).read(), encoding="utf8")
+
 
     if request.method == 'POST':
         main.create_queue(id)
@@ -59,7 +75,7 @@ def create_queue(id):
         else:
             id = '{}'.format(id)
 
-    return render_template('create_queue.html', id=id, qr_pic=qr_pic)
+    return render_template('create_queue.html', id=id)
 
 @app.route('/get_in_queue')
 def get_in_queue():
